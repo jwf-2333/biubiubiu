db054f73b0b9cddb87133e56222bb640a20142e2
jwf <875016526@qq.com>
2020-02-11 16:06:17 +0800

    描述

--- end/project1/bookdemo/booktest/models.py
+++ end/project1/bookdemo/booktest/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=20)
+    price = models.FloatField(default=0)
+    pub_data = models.DateField(default="1983-06-01")
+
+class Hero(models.Model):
+    name = models.CharField(max_length=20)
+    gender = models.CharField(max_length=6,choices=(('male','男'),('female','女')),default='male')
+    content = models.CharField(max_length=100)
+
+    book = models.ForeignKey(Book,on_delete=models.CASCADE)
