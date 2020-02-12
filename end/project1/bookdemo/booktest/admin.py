0aaefc66001240068dadeb582b90391a32ac38db
jwf <875016526@qq.com>
2020-02-12 19:08:54 +0800

    库

--- end/project1/bookdemo/booktest/admin.py
+++ end/project1/bookdemo/booktest/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+# django 自带的后台管理操作需要在此处实现
+
+
+#注册自己需要管理的模型 Book Hero
+from .models import Book,Hero
+admin.site.register(Book)
+admin.site.register(Hero)
+
+
--- end/project1/bookdemo/booktest/models.py
+++ end/project1/bookdemo/booktest/models.py
@@ -7,9 +7,14 @@ class Book(models.Model):
     price = models.FloatField(default=0)
     pub_data = models.DateField(default="1983-06-01")
 
+    def __str__(self):
+        return  self.title
+
 class Hero(models.Model):
     name = models.CharField(max_length=20)
     gender = models.CharField(max_length=6,choices=(('male','男'),('female','女')),default='male')
     content = models.CharField(max_length=100)
 
     book = models.ForeignKey(Book,on_delete=models.CASCADE)
+    def __str__(self):
+        return  self.name
